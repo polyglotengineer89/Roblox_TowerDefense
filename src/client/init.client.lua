b222d6ed13bb189875ff408670493afa9f31264b
muhyasin89 <muhyasin89@gmail.com>
2024-05-27 13:03:45 +0700

    [main]add setup

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,16 @@
-print("Hello world, from client!")
+for i, instance in pairs(game.ClientScriptService::GetDescendants()) do
+	if instance.ClassName ~= "ModuleScript" then continue end
+	_G[instance.Name] = require(instance)
+end
+
+for key, value in pairs(_G) do
+	if typeof(value) ~= "table" then continue end
+	if value.Initialize == nil then continue end
+	value.Initialize()
+end
+
+for key, value in pairs(_G) do
+	if typeof(value) ~= "table" then continue end
+	if value.Initiate == nil then continue end
+	value.Initiate()
+end
